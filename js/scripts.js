5af58a0679d743e08c5f0c0dcbd393a796186167
Jarret Jeter <jarretjeter@yahoo.com>
2022-03-30 13:21:01 -0700

    add function 'qu' to identify if first 2 characters are 'q' and 'u'
    Co-authored-by: Jeremy Martin <jeremyvs1945@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
 function pigLatin(string) {
   const vowels = "a" + "e" + "i" + "o" + "u"
+  const qu = "qu"
   let stringArray = string.toLowerCase().split("");
   let vowelCount = 0
   for (i=0; i < stringArray.length; i++) {
-    console.log(stringArray[i])
+    if (qu.includes(string)) {
+      console.log(string)
+      return 1
+    }
     if (vowels.includes(stringArray[i]))  {
       vowelCount++
     }
   }
   return vowelCount;
 };
+
+function qu(string) {
+  const qu = "qu"
+  let first2chars = string[0].toLowerCase() + string[1].toLowerCase();
+  if (qu === first2chars) {
+    return 1
+  }
+  return -1
+}
