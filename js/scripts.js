ab14b6dc92859d009aa341889c2a6a7a645f7c96
Jarret Jeter <jarretjeter@yahoo.com>
2022-03-30 11:49:18 -0700

    add test case to account for uppercase vowels
    Co-authored-by: Jeremy Martin <jeremyvs1945@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function pigLatin(string) {
   const vowels = "a" + "e" + "i" + "o" + "u"
-  let stringArray = string.split("");
+  let stringArray = string.toLowerCase().split("");
   let vowelCount = 0
   for (i=0; i < stringArray.length; i++) {
     console.log(stringArray[i])
