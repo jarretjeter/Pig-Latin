558b215e55bece09e0bb71abbecb013c5fa44ee3
Jarret Jeter <jarretjeter@yahoo.com>
2022-03-30 14:55:09 -0700

    improve 'hasConsonant function' to place up to first 3 consonant letters to end of string with 'ay' on the end

--- js/scripts.js
+++ js/scripts.js
@@ -13,15 +13,23 @@ function hasConsonant(string) {
   const consonant = "b" + "c" + "d" + "f" + "g" + "h" + "j" + "k" + "l" + "m" + "n" + "p" + "q" + "r" + "s" + "t" + "v" + "w" + "x" + "y" + "z"
   let stringArray = string.toLowerCase().split("");
   let consonantLetter = ""
-  let first2Cons = stringArray[0] + stringArray[1];
-
   if (consonant.includes(stringArray[0]) && consonant.includes(stringArray[1]) && consonant.includes(stringArray[2])) {
-    return 1
+    stringArray.push(stringArray[0] + stringArray[1] + stringArray[2])
+    stringArray.splice(0, 3)
+    consonantLetter = stringArray.join('') + "ay"
+    return consonantLetter
   } else if (consonant.includes(stringArray[0]) && consonant.includes(stringArray[1]))  {
-      return 2
-    } else if (consonant.includes(stringArray[0])) { return 3
+    stringArray.push(stringArray[0] + stringArray[1])
+    stringArray.splice(0, 2)
+    consonantLetter = stringArray.join('') + "ay"
+    return consonantLetter
+    } else if (consonant.includes(stringArray[0])) {
+      stringArray.push(stringArray[0])
+    stringArray.shift();
+    consonantLetter = stringArray.join('') + "ay"
+    return consonantLetter
     }
-  return -1
+  return string
 }
 
 function qu(string) {
